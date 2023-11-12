c969fba5ac399bf86ec9d0c725c0636109e2ffb2
blackfire <blackfire@localhost.localdomain>
2023-11-13 02:29:00 +0500

    console log test updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const app = require('./app');
+const app = require("./app");
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
+  console.log(`Server started at http://localhost:${port}`);
   /* eslint-enable no-console */
 });
